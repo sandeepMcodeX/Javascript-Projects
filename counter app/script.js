2f53e32d3cfe6759e5a110e0df0643a16d7a34d3
sandeepMcodeX <sandeepmcodex@gmail.com>
2025-03-29 08:51:27 +0530

    Update script.js

--- counter app/script.js
+++ counter app/script.js
@@ -1,28 +1,25 @@
-
-const outputValue = document.querySelector('.output-value')
-
-const decreaseButton = document.querySelector('.decreaseButton')
-
-const increaseButton = document.querySelector('.increaseButton')
-
-const userInput = document.querySelector('.userInput')
-
+const decreaseButton = document.querySelector('.minusButton')
+const increaseButton = document.querySelector('.plusButton')
+const outputValue = document.querySelector('.outputValue')
+const userInput = document.querySelector('.input-sty')
 const resetButton = document.querySelector('.resetButton')
 
-decreaseButton.addEventListener('click', () => {
-  const countValue = parseInt(outputValue.innerText);
-  const userInputValue = parseInt(userInput.value);
-  outputValue.innerText = countValue - userInputValue;
+
+decreaseButton.addEventListener('click',()=>{
+   const resultValue = parseInt(outputValue.innerText);
+   const userInputValue=parseInt(userInput.value);
+   outputValue.innerText= resultValue-userInputValue;
+   
 })
 
-increaseButton.addEventListener('click', () => {
-  const countValue = parseInt(outputValue.innerText);
-  const userInputValue = parseInt(userInput.value);
-  outputValue.innerText = countValue + userInputValue;
+increaseButton.addEventListener('click',()=>{
+   const resultValue = parseInt(outputValue.innerText);
+   const userInputValue=parseInt(userInput.value);
+   outputValue.innerText= resultValue+userInputValue;
+
 })
 
-resetButton.addEventListener('click', () => {
-  outputValue.innerText = 0
-  userInput.value=1
-  
+resetButton.addEventListener('click',()=>{
+  outputValue.innerText=0;
+  userInput.value=1;
 })
